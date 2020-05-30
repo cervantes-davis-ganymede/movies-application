be4a8e1c33f45922e00ffc1559491bfba8a1c21a
Laura Davis <laura.l.davis.210@gmail.com>
2020-05-30 01:41:10 -0500

    display loading..., addMovie: reset & update movies table, add/display genre, table styling

--- src/api.js
+++ src/api.js
@@ -3,7 +3,8 @@ module.exports = {
         return fetch('/api/movies')
             .then(response => response.json());
     },
-    addMovies: (movie) => {
+    addMovie: (movie) => {
+        console.log(movie, "<- the movie object to add");
         const url = '/api/movies';
         const options = {
             method: 'POST',
@@ -13,7 +14,7 @@ module.exports = {
             body: JSON.stringify(movie),
         };
         return fetch(url, options)
-            .then(console.log('Post created!'))
+            .then(response => response.json())
             .catch(console.log('error'))
     }
 };
